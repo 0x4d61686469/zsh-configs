6f92dedd3e345f924f29b4dc388f159f50a240b4
0x4d61686469 <iranimahdi79@gmail.com>
2024-12-13 17:29:30 +0330

    add x9 to functions

--- functions.zsh
+++ functions.zsh
@@ -90,3 +90,6 @@ nice_wayback(){
 		echo "$line" | waybackurls | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u | tee ${host}.waybackyrls
 		done < "${1:-/dev/stdin}"
 }
+x9() {                  
+python3 ~/bugbounty-tools/X9/x9.py "$@"
+}
