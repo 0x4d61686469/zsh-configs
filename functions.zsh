5b9dc2c49371a48185e49dbb037e6e66446e0e21
0x4d61686469 <iranimahdi79@gmail.com>
2024-12-23 13:10:45 +0330

    add get_ip_asn

--- functions.zsh
+++ functions.zsh
@@ -191,3 +191,13 @@ source ~/bugbounty-tools/altdns/env/bin/activate
 altdns "$@"
 deactivate
 }
+
+
+
+get_ip_asn() {
+input=""
+while read line
+do
+curl -s https://api.bgpview.io/ip/$line | jq -r ".data.prefixes[0].asn.asn"
+done
+}
