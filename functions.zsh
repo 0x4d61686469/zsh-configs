1120326b3c50a28782e357bb4cfec4ec480a9542
0x4d61686469 <iranimahdi79@gmail.com>
2024-12-29 14:30:02 +0330

    add nice_katana

--- functions.zsh
+++ functions.zsh
@@ -232,7 +232,14 @@ param_maker() {
 }
 
 
-nice_gau(){	
+nice_gau() {	
 	host=$(echo $1 | unfurl format %d)
 	echo "$1" | gau | grep -Eiv '\.(css|jpg|jpeg|png|svg|img|gif|exe|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' | sort -u | tee ${host}.nice_gau
+}
+
+
+
+nice_katana() {
+	host=$(echo $1 | unfurl format %d)
+	echo $1 | katana -js-crawl -known-files all -automatic-form-fill -silent -crawl-scope $host -extension-filter css,jpg,jpeg,png,svg,img,gif,mp4,flv,pdf,doc,ogv,webm,wmv,webp,mov,mp3,m4a,m4p,ppt,pptx,scss,tif,tiff,ttf,otf,woff,woff2,bmp,ico,eot,htc,swf,rtf,image -headers "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
