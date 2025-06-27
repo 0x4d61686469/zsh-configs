d55bf85a8104a758d46171bf2f64aabdfb00be0f
0x4d61686469 <49981891+0x4d61686469@users.noreply.github.com>
2025-06-27 17:33:13 +0330

    Update get_certificate

--- functions.zsh
+++ functions.zsh
@@ -1,5 +1,5 @@
 get_certificate(){
-	echo | openssl s_client -showcerts -servername $1 -connect $1 2>/dev/null | openssl x509 -inform pem -noout -text
+	openssl s_client -showcerts -servername $1 -connect $1:443 2> /dev/null | openssl x509 -inform pem -noout -text
 }
 
 
